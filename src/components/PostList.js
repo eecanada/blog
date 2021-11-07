8668db8f8ac9354df728c9c09a7bf100ed369e6c
Eder Canada <canada.eder@gmail.com>
2021-11-06 23:29:32 -0700

    user id

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -18,6 +18,7 @@ class PostList extends React.Component {
               <h2>{post.title} </h2>
               <p>{post.body}</p>
               <p>{post.id}</p>
+              <p>{post.userId}</p>
             </div>
             <UserHeader userId={post.userId} />
           </div>
